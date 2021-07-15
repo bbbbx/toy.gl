1f1244502a9bb81197e894ccece6b06a749cf17b
bbbbx <venus@venusworld.cn>
2021-07-15 10:49:31 +0800

    feat: add updateTexture

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import createContext from './createContext.js';
 import setState from './setState.js';
 import createFramebuffer from './createFramebuffer.js';
 import createTexture from './createTexture.js';
+import updateTexture from './updateTexture.js';
 import createCubeMap from './createCubeMap.js';
 import clear from './clear.js';
 import draw from './draw.js';
@@ -12,6 +13,7 @@ const ToyGL = {
   clear,
   draw,
   createTexture,
+  updateTexture,
   createCubeMap,
   createFramebuffer,
 };
---
+++ src/updateTexture.js
@@ -0,0 +1,41 @@
+import defaultValue from "./defaultValue";
+
+/**
+ * Update texture data.
+ * @param {WebGLRenderingContext} gl 
+ * @param {WebGLTexture} texture 
+ * @param {Object | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement} source 
+ * @param {Number} source.width 
+ * @param {Number} source.height 
+ * @param {TypedArray} source.arrayBufferView 
+ * @param {Number} [source.level=0] 
+ * @param {Number} [source.internalFormat=gl.RGBA] 
+ * @param {Number} [source.format=gl.RGBA] 
+ * @param {Number} [source.type=gl.UNSIGNED_BYTE] 
+ * @returns {WebGLTexture}
+ */
+function updateTexture(gl, texture, source) {
+  gl.activeTexture(gl.TEXTURE0);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  if (source instanceof HTMLImageElement ||
+    source instanceof HTMLCanvasElement ||
+    source instanceof HTMLVideoElement) {
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, videoElem);
+  } else {
+
+    const { width, height, arrayBufferView } = source;
+    const level = defaultValue(source.level, 0);
+    const internalFormat = defaultValue(source.internalFormat, gl.RGBA);
+    const format = defaultValue(source.format, gl.RGBA);
+    const type = defaultValue(source.internalFormat, gl.UNSIGNED_BYTE);
+    const border = 0;
+
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, format, type, arrayBufferView);
+  }
+
+  gl.bindTexture(gl.TEXTURE_2D, null);
+  return texture;
+}
+
+export default updateTexture;
