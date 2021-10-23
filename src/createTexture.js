19d90c426758e432b8b8589eb62814f1e34b261d
bbbbx <venus@venusworld.cn>
2021-10-23 15:23:50 +0800

    feat(createTexture): support depth texture
    1. `createTexture` can now accept DEPTH_COMPONENT and DEPTH_STENCIL
    format/internalFormat, FLOAT type and support float linear filtering.
    2. Drop `level` parameter, put data in `levels` array or
    `data` if only one level.

--- src/createTexture.js
+++ src/createTexture.js
@@ -1,42 +1,79 @@
 import isPowerOfTwo from './isPowerOfTwo.js';
 import defined from './defined.js';
+import defaultValue from './defaultValue.js';
 
+/**
+ * Create a WebGLTexture.
+ * @memberof ToyGL
+ * @param {WebGLRenderingContext} gl 
+ * @param {Object} options 
+ * @param {Array.<ArrayBufferView | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement>} options.levels all levels data
+ * @param {ArrayBufferView | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement} options.data level 0 data
+ * @param {Number} options.width
+ * @param {Number} options.height
+ * @param {Number} options.internalFormat For WebGL1, internal format must same with format.
+ * @param {Number} options.format
+ * @param {Number} options.type Texel data type, such as <code>gl.UNSIGNED_BYTE</code>, <code>gl.FLOAT</code>, <code>gl.UNSIGNED_INT</code>.
+ * @param {Boolean} [options.generateMipmap=false]
+ * @param {Number} [options.wrapS=CLAMP_TO_EDGE]
+ * @param {Number} [options.wrapT=CLAMP_TO_EDGE]
+ * @param {Number} [options.minFilter=LINEAR]
+ * @param {Number} [options.magFilter=LINEAR]
+ * @returns {WebGLTexture}
+ */
 function createTexture(gl, options) {
-  const { level, internalFormat, type, format, width, height, data, wrapS, wrapT, minFilter, magFilter, generateMipmap } = options;
+  const { internalFormat, type, format, width, height, data, generateMipmap } = options;
+
+  const wrapS = defaultValue(options.wrapS, gl.CLAMP_TO_EDGE);
+  const wrapT = defaultValue(options.wrapT, gl.CLAMP_TO_EDGE);
+  const minFilter = defaultValue(options.minFilter, gl.LINEAR);
+  const magFilter = defaultValue(options.magFilter, gl.LINEAR);
+
+  let levels = options.levels;
+  if (!levels) {
+    levels = [ data ];
+  }
+
   const texture = gl.createTexture();
 
   gl.activeTexture(gl.TEXTURE0);
   gl.bindTexture(gl.TEXTURE_2D, texture);
 
-  if (
-    data instanceof HTMLImageElement ||
-    data instanceof HTMLCanvasElement ||
-    data instanceof HTMLVideoElement
-  ) {
-    gl.texImage2D(gl.TEXTURE_2D, level, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, data);
-  } else {
-    const border = 0;
-    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, format, type, data);
+  if (internalFormat === gl.DEPTH_COMPONENT || gl.DEPTH_STENCIL) {
+    gl.getExtension('WEBGL_depth_texture');
   }
 
-  // default texture settings
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  if (type === gl.FLOAT) {
+    gl.getExtension('OES_texture_float');
+    if (minFilter === gl.LINEAR ||
+      minFilter === gl.LINEAR_MIPMAP_NEAREST ||
+      minFilter === gl.NEAREST_MIPMAP_LINEAR ||
+      minFilter === gl.LINEAR_MIPMAP_LINEAR
+      ) {
+      gl.getExtension('OES_texture_float_linear');
+    }
+  }
 
-  if (defined(wrapS)) {
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, wrapS);
+  const numberOfLevels = levels.length;
+  for (let level = 0; level < numberOfLevels; level++) {
+    const levelData = levels[level];
+
+    if (
+      levelData instanceof HTMLImageElement ||
+      levelData instanceof HTMLCanvasElement ||
+      levelData instanceof HTMLVideoElement
+    ) {
+      gl.texImage2D(gl.TEXTURE_2D, level, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, levelData);
+    } else {
+      const border = 0;
+      gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, format, type, levelData);
+    }
   }
-  if (defined(wrapT)) {
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, wrapT);
-  }
-  if (defined(minFilter)) {
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, minFilter);
-  }
-  if (defined(magFilter)) {
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, magFilter);
-  }
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, wrapS);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, wrapT);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, minFilter);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, magFilter);
 
   if (generateMipmap === true) {
     if (isPowerOfTwo(width) && isPowerOfTwo(height)) {
