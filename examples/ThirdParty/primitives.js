467c0767020757f9ecc4436d03808a75026a1bfb
bbbbx <venus@venusworld.cn>
2023-02-06 01:10:46 +0800

    Add hello example

---
+++ examples/ThirdParty/primitives.js
@@ -0,0 +1,403 @@
+(function() {
+/*
+ * Copyright 2019 Gregg Tavares
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * Low level shader typed array related functions
+ *
+ * You should generally not need to use these functions. They are provided
+ * for those cases where you're doing something out of the ordinary
+ * and you need lower level access.
+ *
+ * For backward compatibility they are available at both `twgl.typedArray` and `twgl`
+ * itself
+ *
+ * See {@link module:twgl} for core functions
+ *
+ * @module twgl/typedArray
+ */
+
+// make sure we don't see a global gl
+const gl = undefined;  /* eslint-disable-line */
+
+/* DataType */
+const BYTE                           = 0x1400;
+const UNSIGNED_BYTE                  = 0x1401;
+const SHORT                          = 0x1402;
+const UNSIGNED_SHORT                 = 0x1403;
+const INT                            = 0x1404;
+const UNSIGNED_INT                   = 0x1405;
+const FLOAT                          = 0x1406;
+const UNSIGNED_SHORT_4_4_4_4       = 0x8033;
+const UNSIGNED_SHORT_5_5_5_1       = 0x8034;
+const UNSIGNED_SHORT_5_6_5         = 0x8363;
+const HALF_FLOAT                   = 0x140B;
+const UNSIGNED_INT_2_10_10_10_REV  = 0x8368;
+const UNSIGNED_INT_10F_11F_11F_REV = 0x8C3B;
+const UNSIGNED_INT_5_9_9_9_REV     = 0x8C3E;
+const FLOAT_32_UNSIGNED_INT_24_8_REV = 0x8DAD;
+const UNSIGNED_INT_24_8            = 0x84FA;
+
+const glTypeToTypedArray = {};
+{
+  const tt = glTypeToTypedArray;
+  tt[BYTE]                           = Int8Array;
+  tt[UNSIGNED_BYTE]                  = Uint8Array;
+  tt[SHORT]                          = Int16Array;
+  tt[UNSIGNED_SHORT]                 = Uint16Array;
+  tt[INT]                            = Int32Array;
+  tt[UNSIGNED_INT]                   = Uint32Array;
+  tt[FLOAT]                          = Float32Array;
+  tt[UNSIGNED_SHORT_4_4_4_4]         = Uint16Array;
+  tt[UNSIGNED_SHORT_5_5_5_1]         = Uint16Array;
+  tt[UNSIGNED_SHORT_5_6_5]           = Uint16Array;
+  tt[HALF_FLOAT]                     = Uint16Array;
+  tt[UNSIGNED_INT_2_10_10_10_REV]    = Uint32Array;
+  tt[UNSIGNED_INT_10F_11F_11F_REV]   = Uint32Array;
+  tt[UNSIGNED_INT_5_9_9_9_REV]       = Uint32Array;
+  tt[FLOAT_32_UNSIGNED_INT_24_8_REV] = Uint32Array;
+  tt[UNSIGNED_INT_24_8]              = Uint32Array;
+}
+
+/**
+ * Get the GL type for a typedArray
+ * @param {ArrayBufferView} typedArray a typedArray
+ * @return {number} the GL type for array. For example pass in an `Int8Array` and `gl.BYTE` will
+ *   be returned. Pass in a `Uint32Array` and `gl.UNSIGNED_INT` will be returned
+ * @memberOf module:twgl/typedArray
+ */
+function getGLTypeForTypedArray(typedArray) {
+  if (typedArray instanceof Int8Array)         { return BYTE; }           // eslint-disable-line
+  if (typedArray instanceof Uint8Array)        { return UNSIGNED_BYTE; }  // eslint-disable-line
+  if (typedArray instanceof Uint8ClampedArray) { return UNSIGNED_BYTE; }  // eslint-disable-line
+  if (typedArray instanceof Int16Array)        { return SHORT; }          // eslint-disable-line
+  if (typedArray instanceof Uint16Array)       { return UNSIGNED_SHORT; } // eslint-disable-line
+  if (typedArray instanceof Int32Array)        { return INT; }            // eslint-disable-line
+  if (typedArray instanceof Uint32Array)       { return UNSIGNED_INT; }   // eslint-disable-line
+  if (typedArray instanceof Float32Array)      { return FLOAT; }          // eslint-disable-line
+  throw new Error('unsupported typed array type');
+}
+
+/**
+ * Get the GL type for a typedArray type
+ * @param {ArrayBufferView} typedArrayType a typedArray constructor
+ * @return {number} the GL type for type. For example pass in `Int8Array` and `gl.BYTE` will
+ *   be returned. Pass in `Uint32Array` and `gl.UNSIGNED_INT` will be returned
+ * @memberOf module:twgl/typedArray
+ */
+function getGLTypeForTypedArrayType(typedArrayType) {
+  if (typedArrayType === Int8Array)         { return BYTE; }           // eslint-disable-line
+  if (typedArrayType === Uint8Array)        { return UNSIGNED_BYTE; }  // eslint-disable-line
+  if (typedArrayType === Uint8ClampedArray) { return UNSIGNED_BYTE; }  // eslint-disable-line
+  if (typedArrayType === Int16Array)        { return SHORT; }          // eslint-disable-line
+  if (typedArrayType === Uint16Array)       { return UNSIGNED_SHORT; } // eslint-disable-line
+  if (typedArrayType === Int32Array)        { return INT; }            // eslint-disable-line
+  if (typedArrayType === Uint32Array)       { return UNSIGNED_INT; }   // eslint-disable-line
+  if (typedArrayType === Float32Array)      { return FLOAT; }          // eslint-disable-line
+  throw new Error('unsupported typed array type');
+}
+
+/**
+ * Get the typed array constructor for a given GL type
+ * @param {number} type the GL type. (eg: `gl.UNSIGNED_INT`)
+ * @return {function} the constructor for a the corresponding typed array. (eg. `Uint32Array`).
+ * @memberOf module:twgl/typedArray
+ */
+function getTypedArrayTypeForGLType(type) {
+  const CTOR = glTypeToTypedArray[type];
+  if (!CTOR) {
+    throw new Error('unknown gl type');
+  }
+  return CTOR;
+}
+
+const isArrayBuffer = typeof SharedArrayBuffer !== 'undefined'
+  ? function isArrayBufferOrSharedArrayBuffer(a) {
+    return a && a.buffer && (a.buffer instanceof ArrayBuffer || a.buffer instanceof SharedArrayBuffer);
+  }
+  : function isArrayBuffer(a) {
+    return a && a.buffer && a.buffer instanceof ArrayBuffer;
+  };
+
+const typedArrays = {
+  getGLTypeForTypedArray,
+  getGLTypeForTypedArrayType,
+  getTypedArrayTypeForGLType,
+  isArrayBuffer,
+};
+
+
+/**
+ * Creates sphere vertices.
+ *
+ * The created sphere has position, normal, and texcoord data
+ *
+ * @param {number} radius radius of the sphere.
+ * @param {number} subdivisionsAxis number of steps around the sphere.
+ * @param {number} subdivisionsHeight number of vertically on the sphere.
+ * @param {number} [opt_startLatitudeInRadians] where to start the
+ *     top of the sphere. Default = 0.
+ * @param {number} [opt_endLatitudeInRadians] Where to end the
+ *     bottom of the sphere. Default = Math.PI.
+ * @param {number} [opt_startLongitudeInRadians] where to start
+ *     wrapping the sphere. Default = 0.
+ * @param {number} [opt_endLongitudeInRadians] where to end
+ *     wrapping the sphere. Default = 2 * Math.PI.
+ * @return {Object.<string, TypedArray>} The created sphere vertices.
+ * @memberOf module:twgl/primitives
+ */
+function createSphereVertices(
+  radius,
+  subdivisionsAxis,
+  subdivisionsHeight,
+  opt_startLatitudeInRadians,
+  opt_endLatitudeInRadians,
+  opt_startLongitudeInRadians,
+  opt_endLongitudeInRadians) {
+  if (subdivisionsAxis <= 0 || subdivisionsHeight <= 0) {
+    throw new Error('subdivisionAxis and subdivisionHeight must be > 0');
+  }
+
+  opt_startLatitudeInRadians = opt_startLatitudeInRadians || 0;
+  opt_endLatitudeInRadians = opt_endLatitudeInRadians || Math.PI;
+  opt_startLongitudeInRadians = opt_startLongitudeInRadians || 0;
+  opt_endLongitudeInRadians = opt_endLongitudeInRadians || (Math.PI * 2);
+
+  const latRange = opt_endLatitudeInRadians - opt_startLatitudeInRadians;
+  const longRange = opt_endLongitudeInRadians - opt_startLongitudeInRadians;
+
+  // We are going to generate our sphere by iterating through its
+  // spherical coordinates and generating 2 triangles for each quad on a
+  // ring of the sphere.
+  const numVertices = (subdivisionsAxis + 1) * (subdivisionsHeight + 1);
+  const positions = createAugmentedTypedArray(3, numVertices);
+  const normals   = createAugmentedTypedArray(3, numVertices);
+  const texcoords = createAugmentedTypedArray(2, numVertices);
+
+  // Generate the individual vertices in our vertex buffer.
+  for (let y = 0; y <= subdivisionsHeight; y++) {
+    for (let x = 0; x <= subdivisionsAxis; x++) {
+      // Generate a vertex based on its spherical coordinates
+      const u = x / subdivisionsAxis;
+      const v = y / subdivisionsHeight;
+      const theta = longRange * u + opt_startLongitudeInRadians;
+      const phi = latRange * v + opt_startLatitudeInRadians;
+      const sinTheta = Math.sin(theta);
+      const cosTheta = Math.cos(theta);
+      const sinPhi = Math.sin(phi);
+      const cosPhi = Math.cos(phi);
+      const ux = cosTheta * sinPhi;
+      const uy = cosPhi;
+      const uz = sinTheta * sinPhi;
+      positions.push(radius * ux, radius * uy, radius * uz);
+      normals.push(ux, uy, uz);
+      texcoords.push(1 - u, v);
+    }
+  }
+
+  const numVertsAround = subdivisionsAxis + 1;
+  const indices = createAugmentedTypedArray(3, subdivisionsAxis * subdivisionsHeight * 2, Uint16Array);
+  for (let x = 0; x < subdivisionsAxis; x++) {  // eslint-disable-line
+    for (let y = 0; y < subdivisionsHeight; y++) {  // eslint-disable-line
+      // Make triangle 1 of quad.
+      indices.push(
+          (y + 0) * numVertsAround + x,
+          (y + 0) * numVertsAround + x + 1,
+          (y + 1) * numVertsAround + x);
+
+      // Make triangle 2 of quad.
+      indices.push(
+          (y + 1) * numVertsAround + x,
+          (y + 0) * numVertsAround + x + 1,
+          (y + 1) * numVertsAround + x + 1);
+    }
+  }
+
+  return {
+    position: positions,
+    normal: normals,
+    texcoord: texcoords,
+    indices: indices,
+  };
+}
+
+const CUBE_FACE_INDICES = [
+  [3, 7, 5, 1],  // right
+  [6, 2, 0, 4],  // left
+  [6, 7, 3, 2],  // ??
+  [0, 1, 5, 4],  // ??
+  [7, 6, 4, 5],  // front
+  [2, 3, 1, 0],  // back
+];
+
+/**
+ * Creates the buffers and indices for a cube.
+ *
+ * The cube is created around the origin. (-size / 2, size / 2).
+ *
+ * @param {WebGLRenderingContext} gl The WebGLRenderingContext.
+ * @param {number} [size] width, height and depth of the cube.
+ * @return {Object.<string, WebGLBuffer>} The created buffers.
+ * @memberOf module:twgl/primitives
+ * @function createCubeBuffers
+ */
+
+/**
+ * Creates the vertices and indices for a cube.
+ *
+ * The cube is created around the origin. (-size / 2, size / 2).
+ *
+ * @param {number} [size] width, height and depth of the cube.
+ * @return {Object.<string, TypedArray>} The created vertices.
+ * @memberOf module:twgl/primitives
+ */
+function createCubeVertices(size) {
+  size = size || 1;
+  const k = size / 2;
+
+  const cornerVertices = [
+    [-k, -k, -k],
+    [+k, -k, -k],
+    [-k, +k, -k],
+    [+k, +k, -k],
+    [-k, -k, +k],
+    [+k, -k, +k],
+    [-k, +k, +k],
+    [+k, +k, +k],
+  ];
+
+  const faceNormals = [
+    [+1, +0, +0],
+    [-1, +0, +0],
+    [+0, +1, +0],
+    [+0, -1, +0],
+    [+0, +0, +1],
+    [+0, +0, -1],
+  ];
+
+  const uvCoords = [
+    [1, 0],
+    [0, 0],
+    [0, 1],
+    [1, 1],
+  ];
+
+  const numVertices = 6 * 4;
+  const positions = createAugmentedTypedArray(3, numVertices);
+  const normals   = createAugmentedTypedArray(3, numVertices);
+  const texcoords = createAugmentedTypedArray(2 , numVertices);
+  const indices   = createAugmentedTypedArray(3, 6 * 2, Uint16Array);
+
+  for (let f = 0; f < 6; ++f) {
+    const faceIndices = CUBE_FACE_INDICES[f];
+    for (let v = 0; v < 4; ++v) {
+      const position = cornerVertices[faceIndices[v]];
+      const normal = faceNormals[f];
+      const uv = uvCoords[v];
+
+      // Each face needs all four vertices because the normals and texture
+      // coordinates are not all the same.
+      positions.push(position);
+      normals.push(normal);
+      texcoords.push(uv);
+
+    }
+    // Two triangles make a square face.
+    const offset = 4 * f;
+    indices.push(offset + 0, offset + 1, offset + 2);
+    indices.push(offset + 0, offset + 2, offset + 3);
+  }
+
+  return {
+    position: positions,
+    normal: normals,
+    texcoord: texcoords,
+    indices: indices,
+  };
+}
+
+/**
+ * creates a typed array with a `push` function attached
+ * so that you can easily *push* values.
+ *
+ * `push` can take multiple arguments. If an argument is an array each element
+ * of the array will be added to the typed array.
+ *
+ * Example:
+ *
+ *     const array = createAugmentedTypedArray(3, 2);  // creates a Float32Array with 6 values
+ *     array.push(1, 2, 3);
+ *     array.push([4, 5, 6]);
+ *     // array now contains [1, 2, 3, 4, 5, 6]
+ *
+ * Also has `numComponents` and `numElements` properties.
+ *
+ * @param {number} numComponents number of components
+ * @param {number} numElements number of elements. The total size of the array will be `numComponents * numElements`.
+ * @param {constructor} opt_type A constructor for the type. Default = `Float32Array`.
+ * @return {ArrayBufferView} A typed array.
+ * @memberOf module:twgl/primitives
+ */
+function createAugmentedTypedArray(numComponents, numElements, opt_type) {
+  const Type = opt_type || Float32Array;
+  return augmentTypedArray(new Type(numComponents * numElements), numComponents);
+}
+
+/**
+ * Add `push` to a typed array. It just keeps a 'cursor'
+ * and allows use to `push` values into the array so we
+ * don't have to manually compute offsets
+ * @param {TypedArray} typedArray TypedArray to augment
+ * @param {number} numComponents number of components.
+ * @private
+ */
+function augmentTypedArray(typedArray, numComponents) {
+  let cursor = 0;
+  typedArray.push = function() {
+    for (let ii = 0; ii < arguments.length; ++ii) {
+      const value = arguments[ii];
+      if (value instanceof Array || typedArrays.isArrayBuffer(value)) {
+        for (let jj = 0; jj < value.length; ++jj) {
+          typedArray[cursor++] = value[jj];
+        }
+      } else {
+        typedArray[cursor++] = value;
+      }
+    }
+  };
+  typedArray.reset = function(opt_index) {
+    cursor = opt_index || 0;
+  };
+  typedArray.numComponents = numComponents;
+  Object.defineProperty(typedArray, 'numElements', {
+    get: function() {
+      return this.length / this.numComponents | 0;
+    },
+  });
+  return typedArray;
+}
+
+window.primitives = {
+  createSphereVertices,
+  createCubeVertices,
+};
+})();
